6546f58adb92e3321a47ebafcf75d9b16ded685b
suren777 <suren.islyaev@gmail.com>
2014-07-23 11:56:37 +0100

    modified:   cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R is set of two functions to evaluaate a matrix inverse
+## function 'makeCacheMatrix' takes a matrix as an argument and creates
+## an object like output, which stores the matrix itself, its inverse
+## and four methods for operating with this object;
+## function 'cacheSolve' first performs check if inverse of the matrix defined in the previous function
+## is already exists, if not, it evaluates an inverse
 
-## Write a short comment describing this function
+
+## makeCacheMatrix take as input matrix x, and outputs a list of methods
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    xInv <- NULL            ## set a NULL value for an inverse matrix
+    set <- function(y) {    ## method to update components of this object
+        x <<- y             ## update x
+        xInv <<- NULL       ## update inverse of x
+    }
+    get <- function() x     ## method to get a data from an object
+    setinv <- function(inv) xInv <<- inv ## method to update an inverse value of x
+    getinv <- function() xInv            ## method to retrieve an inverse value of x from cache 
+    list(set = set, get = get,           ## output
+         setinv = setinv,
+         getinv = getinv)
 }
+ 
 
 
-## Write a short comment describing this function
+
+## function 'cacheSolve' takes as input an object generated with above function
+## and outputs inverse of matrix, stored in this object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+    xInv <- x$getinv() ## check if inverse is already evaluated
+    if(!is.null(xInv)) {
+        message("getting cached data")  ## if yes, print the message and return the matrix inverse
+        return(xInv)
+    }
+    matrix <- x$get() ## if not, get the value of the matrix from the object x
+    xInv <- solve(matrix, ...) ## evaluate the inverse
+    x$setinv(xInv) ## store inverse inside the object
+    xInv ## return the inverse
+    
+    
 }
+
